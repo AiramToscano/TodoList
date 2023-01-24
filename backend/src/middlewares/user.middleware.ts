431936ad6ab5d515a1f4c9827a510e392439bc1a
Airam Toscano <airamlobato@gmail.com>
2023-01-24 10:58:28 -0400

    fix: add space not allowd middlewares users

--- backend/src/middlewares/user.middleware.ts
+++ backend/src/middlewares/user.middleware.ts
@@ -5,11 +5,15 @@ import { Request, Response, NextFunction } from 'express';
 export class UsersMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { password, username } = req.body;
+    const verificaEspaco = /\s/g.test(username);
     if (!password || !username) {
       return res.status(400).json({ message: 'username or password required' });
     }
     if (password.length < 3 || username < 3) {
       return res.status(400).json({ message: 'username or password shorts' });
+    }
+    if (verificaEspaco) {
+      return res.status(400).json({ message: 'space not allowed' });
     }
     next();
   }
