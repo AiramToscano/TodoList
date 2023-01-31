31613a8d0189a260852e5c0d240f885f7302030f
Airam Toscano <airamlobato@gmail.com>
2023-01-31 11:54:48 -0400

    Fix: docker-compose

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import swaggerDocs from '../swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
   await app.listen(3001);
 }
