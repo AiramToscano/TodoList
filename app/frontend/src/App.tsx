a3102e4c1e89007380f9f6f643f6c4e9ec456f0d
Airam Toscano <airamlobato@gmail.com>
2023-02-03 11:41:13 -0400

    feat: add pages home/login

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
     <Routes>
+      <Route path="/home" element={ <Home /> } />
       <Route path="/login" element={ <Login /> } />
       <Route path="/" element={ <Navigate replace to="/login" /> } />
     </Routes>
